7f7d88029b03abcd765579e4b3c6599b10aa909f
Ian Pointer <ipointer@Ians-MacBook-Pro.local>
2015-06-18 08:42:48 -0400

    Fix logo CSS

--- monte_carlo_streaming/monte-server.rb
+++ monte_carlo_streaming/monte-server.rb
@@ -4,10 +4,10 @@ require 'bigdecimal'
 require 'csv'
 require 'open-uri'
 require 'nokogiri'
-#require 'poseidon'
+require 'poseidon'
 require 'pry'
 
-#$kafka = Poseidon::Producer.new([ENV['KAFKA_HOST']], "finance-demo")
+$kafka = Poseidon::Producer.new([ENV['KAFKA_HOST']], "finance-demo")
 
 class DataGrid
  
@@ -96,7 +96,7 @@ post '/spark' do
   puts JSON.parse(params['submission'])
    submission = params['submission']
    msg = Poseidon::MessageToSend.new("submissions", submission)
- #  $kafka.send_messages([msg])
+   $kafka.send_messages([msg])
 #   {status: 'progress'}.to_json
   params['submission']
 end
